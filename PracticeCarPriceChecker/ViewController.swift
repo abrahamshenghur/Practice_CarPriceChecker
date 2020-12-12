7f1e2b2a34eb0c0e3f29673be2d40b8344d21d73
abrahamshenghur <ashenghur@gmail.com>
2020-12-12 01:32:12 -0800

    Initial commit

---
+++ PracticeCarPriceChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeCarPriceChecker
+//
+//  Created by John on 12/12/20.
+//  Copyright © 2020 Abraham Shenghur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
